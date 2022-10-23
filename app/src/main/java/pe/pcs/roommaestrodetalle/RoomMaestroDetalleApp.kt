a0dc9d0bc43f5f2acef95f93750510ad78e526b6
Jack Chávez Saravia <jacksoft@live.com>
2022-10-23 14:31:56 -0500

    Correccion en RoomMaestroDetalleApp.kt Version 1.0.7

--- app/src/main/java/pe/pcs/roommaestrodetalle/RoomMaestroDetalleApp.kt
+++ app/src/main/java/pe/pcs/roommaestrodetalle/RoomMaestroDetalleApp.kt
@@ -2,10 +2,8 @@ package pe.pcs.roommaestrodetalle
 
 import android.app.Application
 import android.content.Context
-import androidx.room.Room
 import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
-import pe.pcs.roommaestrodetalle.data.database.AppDatabase
 
 @HiltAndroidApp
 class RoomMaestroDetalleApp: Application() {
@@ -22,7 +20,6 @@ class RoomMaestroDetalleApp: Application() {
         instancia = this
     }
 
-    // Crea la DB al iniciar la app
     override fun onCreate() {
         super.onCreate()
 
